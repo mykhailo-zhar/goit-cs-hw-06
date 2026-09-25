14a3045c5675f05292b6fae5de1b9394b7335d68
Mykhailo Zhar <mykhailo.zhar.od.ua@gmail.com>
2026-09-25 16:03:57 +0300

    Add no serv link support

--- src/db.py
+++ src/db.py
@@ -10,13 +10,29 @@ def create_mongo_connection():
     Credentials are read from MONGODB_USER, MONGODB_PASSWORD, MONGODB_HOST,
     and MONGODB_APPNAME.
     """
-    client = MongoClient(
-        "mongodb+srv://{username}:{password}@{host}?appName={db}&retryWrites=true&w=majority".format(
+    port = os.environ.get("MONGODB_PORT")
+    app_name = os.environ.get("MONGODB_APPNAME")
+
+    db = f"appName={app_name}" if app_name else ""
+
+    if not os.environ.get("MONGODB_NOSERV"):
+        connection_string = "mongodb+srv://{username}:{password}@{host}?{db}retryWrites=true&w=majority".format(
             username=os.environ.get("MONGODB_USER"),
             password=os.environ.get("MONGODB_PASSWORD"),
             host=os.environ.get("MONGODB_HOST"),
-            db=os.environ.get("MONGODB_APPNAME"),
-        ),
+            db=db,
+        )
+    else:
+        connection_string = "mongodb://{username}:{password}@{host}{port}?{db}directConnection=true".format(
+            username=os.environ.get("MONGODB_USER"),
+            password=os.environ.get("MONGODB_PASSWORD"),
+            host=os.environ.get("MONGODB_HOST"),
+            db=db,
+            port=f":{port}" if port else "",
+        )
+
+    client = MongoClient(
+        connection_string,
         server_api=ServerApi("1"),
     )
     db = client.messages
